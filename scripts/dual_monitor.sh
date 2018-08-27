4706f1d95cfd34b10677108d1b8254f761e639fa
0x113 <jose.guombinho@gmail.com>
2018-08-27 12:21:10 +0200

    :lipstick: changed brightness of second screen

--- scripts/dual_monitor.sh
+++ scripts/dual_monitor.sh
@@ -3,7 +3,7 @@
 # laptop monitor
 xrandr --output LVDS1 --mode 1366x768 --rate 60
 
-xrandr --output HDMI2 --mode 1360x768 --rate 59.80 --brightness 0.30
+xrandr --output HDMI2 --mode 1360x768 --rate 59.80 --brightness 0.35
 
 xrandr --output LVDS1 --left-of HDMI2
 xrandr --output LVDS1 --primary
