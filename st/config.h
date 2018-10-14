3e2bd29958c88b4eb5b97857c46d4831f8fb1ba7
0x113 <jose.guombinho@gmail.com>
2018-10-14 20:42:59 +0200

    :lipstick: changed colors

--- st/config.h
+++ st/config.h
@@ -111,32 +111,63 @@ unsigned int tabspaces = 8;
 //  [257] = "#ffffff", /* foreground */
 //};
 
-static const char *colorname[] = {
+//static const char *colorname[] = {
+//
+//  /* 8 normal colors */
+//  [0] = "#032c36", /* black   */
+//  [1] = "#c2454e", /* red     */
+//  [2] = "#7cbf9e", /* green   */
+//  [3] = "#8a7a63", /* yellow  */
+//  [4] = "#2e3340", /* blue    */
+//  [5] = "#ff5879", /* magenta */
+//  [6] = "#44b5b1", /* cyan    */
+//  [7] = "#f2f1b9", /* white   */
+//
+//  /* 8 bright colors */
+//  [8]  = "#065f73", /* black   */
+//  [9]  = "#ef5847", /* red     */
+//  [10] = "#a2d9b1", /* green   */
+//  [11] = "#beb090", /* yellow  */
+//  [12] = "#61778d", /* blue    */
+//  [13] = "#ff99a1", /* magenta */
+//  [14] = "#9ed9d8", /* cyan    */
+//  [15] = "#f6f6c9", /* white   */
+//
+//  /* special colors */
+//  [256] = "#071D22", /* background */
+//  [257] = "#e8dfd6", /* foreground */
+//};
+
+
+const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#032c36", /* black   */
-  [1] = "#c2454e", /* red     */
-  [2] = "#7cbf9e", /* green   */
-  [3] = "#8a7a63", /* yellow  */
-  [4] = "#2e3340", /* blue    */
-  [5] = "#ff5879", /* magenta */
-  [6] = "#44b5b1", /* cyan    */
-  [7] = "#f2f1b9", /* white   */
+  [0] = "#090909", /* black   */
+  [1] = "#64655A", /* red     */
+  [2] = "#70716F", /* green   */
+  [3] = "#817D77", /* yellow  */
+  [4] = "#7C8372", /* blue    */
+  [5] = "#848679", /* magenta */
+  [6] = "#767985", /* cyan    */
+  [7] = "#b5b8bb", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#065f73", /* black   */
-  [9]  = "#ef5847", /* red     */
-  [10] = "#a2d9b1", /* green   */
-  [11] = "#beb090", /* yellow  */
-  [12] = "#61778d", /* blue    */
-  [13] = "#ff99a1", /* magenta */
-  [14] = "#9ed9d8", /* cyan    */
-  [15] = "#f6f6c9", /* white   */
+  [8]  = "#7e8082",  /* black   */
+  [9]  = "#64655A",  /* red     */
+  [10] = "#70716F", /* green   */
+  [11] = "#817D77", /* yellow  */
+  [12] = "#7C8372", /* blue    */
+  [13] = "#848679", /* magenta */
+  [14] = "#767985", /* cyan    */
+  [15] = "#b5b8bb", /* white   */
 
   /* special colors */
-  [256] = "#071D22", /* background */
-  [257] = "#e8dfd6", /* foreground */
+  [256] = "#090909", /* background */
+  [257] = "#b5b8bb", /* foreground */
+  [258] = "#b5b8bb",     /* cursor */
 };
+
+
 
 /*
  * Default colors (colorname index)
@@ -145,7 +176,6 @@ static const char *colorname[] = {
 unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 257;
-
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
  * will reverse too. Another logic would only make the simple feature too
