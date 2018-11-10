36e24c36d49fabad1a1209aec3fa5ced6c5b37f3
0x113 <jose.guombinho@gmail.com>
2018-11-10 18:40:58 +0100

    :boom: new bar

--- scripts/launch_polybar.sh
+++ scripts/launch_polybar.sh
@@ -1,6 +1,6 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload main &
   done
 else
   polybar --reload example &
