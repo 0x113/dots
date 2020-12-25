dc724e78d17a9d7232561fd96ac96f5a52adc16d
0x113 <jose.guombinho@gmail.com>
2020-12-25 20:29:08 +0100

    changed font and colors

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "scientifica:pixelsize=11:antialias=false:autohint=true";
+static char *font = "JetBrains Mono Medium:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 30;
 
 /*
@@ -85,29 +85,57 @@ unsigned int tabspaces = 8;
 static const char *colorname[] = {
 
   /* 8 normal colors */
-  [0] = "#032c36", /* black   */
-  [1] = "#c2454e", /* red     */
-  [2] = "#7cbf9e", /* green   */
-  [3] = "#8a7a63", /* yellow  */
-  [4] = "#2e3340", /* blue    */
-  [5] = "#ff5879", /* magenta */
-  [6] = "#44b5b1", /* cyan    */
-  [7] = "#f2f1b9", /* white   */
+  [0] = "#1d1f21", /* black   */
+  [1] = "#cc6666", /* red     */
+  [2] = "#b5bd68", /* green   */
+  [3] = "#f0c674", /* yellow  */
+  [4] = "#81a2be", /* blue    */
+  [5] = "#b294bb", /* magenta */
+  [6] = "#8abeb7", /* cyan    */
+  [7] = "#c5c8c6", /* white   */
 
   /* 8 bright colors */
-  [8]  = "#065f73", /* black   */
-  [9]  = "#ef5847", /* red     */
-  [10] = "#a2d9b1", /* green   */
-  [11] = "#beb090", /* yellow  */
-  [12] = "#61778d", /* blue    */
-  [13] = "#ff99a1", /* magenta */
-  [14] = "#9ed9d8", /* cyan    */
-  [15] = "#f6f6c9", /* white   */
+  [8]  = "#969896", /* black   */
+  [9]  = "#cc6666", /* red     */
+  [10] = "#b5bd68", /* green   */
+  [11] = "#f0c674", /* yellow  */
+  [12] = "#81a2be", /* blue    */
+  [13] = "#b294bb", /* magenta */
+  [14] = "#8abeb7", /* cyan    */
+  [15] = "#ffffff", /* white   */
 
   /* special colors */
-  [256] = "#142c35", /* background */
-  [257] = "#e8dfd6", /* foreground */
+  [256] = "#1d1f21", /* background */
+  [257] = "#c5c8c6", /* foreground */
 };
+
+
+//static const char *colorname[] = {
+//
+//  /* 8 normal colors */
+//  [0] = "#032c36", /* black   */
+//  [1] = "#c2454e", /* red     */
+//  [2] = "#7cbf9e", /* green   */
+//  [3] = "#8a7a63", /* yellow  */
+//  [4] = "#2e3340", /* blue    */
+//  [5] = "#ff5879", /* magenta */
+//  [6] = "#44b5b1", /* cyan    */
+//  [7] = "#f2f1b9", /* white   */
+//
+//  /* 8 bright colors */
+//  [8]  = "#065f73", /* black   */
+//  [9]  = "#ef5847", /* red     */
+//  [10] = "#a2d9b1", /* green   */
+//  [11] = "#beb090", /* yellow  */
+//  [12] = "#61778d", /* blue    */
+//  [13] = "#ff99a1", /* magenta */
+//  [14] = "#9ed9d8", /* cyan    */
+//  [15] = "#f6f6c9", /* white   */
+//
+//  /* special colors */
+//  [256] = "#142c35", /* background */
+//  [257] = "#e8dfd6", /* foreground */
+//};
 
 /*
  * Default colors (colorname index)
